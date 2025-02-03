f365f0a2401ab16df58f532542fe9cb24efe0850
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-02-03 11:45:14 +0100

    feat(repository): add JPA repositories (User, Event, Comment)

---
+++ CorpoEvent/src/main/java/org/example/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.example.repository;
+
+import org.example.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.events WHERE u.email = :email")
+    User findByEmail(@Param("email") String email);
+} 
