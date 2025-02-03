6be5e36086f15b3a0cf48c758ea82819827631d7
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-02-03 13:06:27 +0100

    fix(config): add NonNull annotation to DateTimeConfig

--- CorpoEvent/src/main/java/org/example/config/DateTimeConfig.java
+++ CorpoEvent/src/main/java/org/example/config/DateTimeConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 import java.time.format.DateTimeFormatter;
 
@@ -11,7 +12,7 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeConfig implements WebMvcConfigurer {
 
     @Override
-    public void addFormatters(FormatterRegistry registry) {
+    public void addFormatters(@NonNull FormatterRegistry registry) {
         DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
         registrar.setDateTimeFormatter(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         registrar.registerFormatters(registry);
